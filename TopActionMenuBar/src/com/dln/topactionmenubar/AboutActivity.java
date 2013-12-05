9b5e7e62307a13217cf1f01429d0de8b2a754038
Daniel <micartero@gmail.com>
2013-12-05 13:53:06 +0100

    Added new Images and menu items

---
+++ TopActionMenuBar/src/com/dln/topactionmenubar/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.dln.topactionmenubar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class AboutActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+
+	/*@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}*/
+
+}
