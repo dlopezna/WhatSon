751d04be02585efd2d19a4887637a719562ed4d4
Daniel <micartero@gmail.com>
2013-12-05 13:46:19 +0100

    Added new icons and menu elements

--- TopActionMenuBar/src/com/dln/topactionmenubar/MainActivity.java
+++ TopActionMenuBar/src/com/dln/topactionmenubar/MainActivity.java
@@ -2,7 +2,11 @@ package com.dln.topactionmenubar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +24,36 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void showAbout() {
+        Intent intent = new Intent(this, AboutActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    
+    public void showMessages() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        
+        
+    }
+   @Override
+public boolean onOptionsItemSelected(MenuItem item) {
+	// TODO Auto-generated method stub
+
+	switch (item.getItemId()) {
+    case R.id.action_about:
+    	showAbout();
+        return true;
+    case R.id.action_messages:
+    	showMessages();
+        return true;
+    default:
+        return super.onOptionsItemSelected(item);
+	}
+   }
 }
